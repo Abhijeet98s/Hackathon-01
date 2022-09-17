b42a61776acbbacf0770840635c0420874ad4a0a
Abhijeet98s <abhi.21smiley00@gmail.com>
2022-09-17 21:59:08 +0530

    added sound feature

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // fetching data from local storage
 const pomodoroTime = JSON.parse(localStorage.getItem("timerArr"))[0]
-
 const pomodoroArray = JSON.parse(localStorage.getItem("timerArr"))
+console.log(pomodoroArray.length);
 
 // Timer
 let minHand = document.getElementById("min");
@@ -17,6 +17,13 @@ let pauseButton = document.getElementById("pause");
 let pomoTimer = document.getElementById("pomo")
 let breakTimer = document.getElementById("break")
 
+// Sounds
+let bell = new Audio("./bell.mp3")
+let click = new Audio("./click.mp3")
+
+// let info = document.getElementById("info-container")
+
+
 let startTimer;
 
 window.addEventListener("keyup", handleKeyPress);
@@ -25,15 +32,18 @@ time.addEventListener("load", setValue());
 
 
 function setValue() {
-    minHand.innerText = pomodoroArray.length ? pomodoroTime.min : "10";
+    minHand.innerText = pomodoroArray ? pomodoroTime.min : "10";
     secHand.innerText = "00";
     breakTimer.innerText = "";
+    // info.innerHTML = `<div> Pomodoro </div>`
 }
 
 function startBreak() {
     minHand.innerText = pomodoroArray.length ? pomodoroTime.brTime : "05";
     secHand.innerText = "00";
     pomoTimer.innerText = "Break";
+    // pomoTimer.style.display = "none"
+    // breakTimer.style.display = "block"
 }
 
 
@@ -50,6 +60,7 @@ function handleKeyPress() {
 
 // Start the timer
 startButton.addEventListener("click", function () {
+    click.play()
     if (startTimer === undefined) {
         startTimer = setInterval(timer, 1000);
     }
@@ -73,7 +84,7 @@ function timer() {
         }
     }
     else if (minHand.innerText == 0 && secHand.innerText == 0) {
-        // bell.play();
+        bell.play();
         alert("Time is up");
         startBreak();
         clearInterval(startTimer);
@@ -87,7 +98,6 @@ pauseButton.addEventListener("click", function () {
     clearInterval(startTimer);
     startTimer = undefined;
 });
-
 
 
 // Reset the timer
